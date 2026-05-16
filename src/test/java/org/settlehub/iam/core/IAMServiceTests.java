5c55eb57c7f8af7a115e63bd9a49bb1573b87bc3
Vladyslav Karakai <hitechnic00074220@gmail.com>
2026-05-17 00:56:14 +0300

    add CI/CD scripts (#1)
    * add CI script
    
    * change the ci.yml
    
    * resolve SpringBootTest context loading issue by fixing package structure
    
    * reconfigure test package
    
    * reconfigure test application-test.yml
    
    * reconfigure CI pipeline
    
    * reconfigure CI pipeline
    
    * reconfigure CI pipeline
    
    * add Docker Compose File
    
    * add CD pipeline

---
+++ src/test/java/org/settlehub/iam/core/IAMServiceTests.java
@@ -0,0 +1,14 @@
+package org.settlehub.iam.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class IAMServiceTests {
+
+    @Test
+    void contextLoads() { }
+
+}
